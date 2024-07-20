69bb5bd3523f66dcd9edac98d4a63d6586e07920
津口雄作 <tsuguchi@nnn.or.jp>
2024-07-20 18:13:51 +0900

    commit

--- src/main/java/org/example/plugin/plugin/Plugin.java
+++ src/main/java/org/example/plugin/plugin/Plugin.java
@@ -1,19 +1,34 @@
 package org.example.plugin.plugin;
 
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Plugin extends JavaPlugin {
+public final class Plugin extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         getServer().getPluginCommand("player").setExecutor(new Commands());
-        PlayerInventory inventory = getServer().getPlayer("player").getInventory();
-        getServer().getPlayer("player").getInventory();
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        PlayerInventory inventory = player.getInventory();
+
 
         ItemStack helmet = new ItemStack(Material.NETHERITE_HELMET);
         inventory.setItem(EquipmentSlot.HEAD, helmet);
@@ -30,15 +45,13 @@ public final class Plugin extends JavaPlugin {
         ItemStack sword = new ItemStack(Material.NETHERITE_SWORD);
         inventory.setItem(EquipmentSlot.HAND, sword);
 
+        ItemStack pickaxe = new ItemStack(Material.NETHERITE_PICKAXE);
+        inventory.setItem(EquipmentSlot.OFF_HAND, pickaxe);
+
         ItemStack shield = new ItemStack(Material.SHIELD);
-        inventory.setItem(EquipmentSlot.OFF_HAND, shield);
+        inventory.setItem(EquipmentSlot.HAND, shield);
 
-        ItemStack beaf = new ItemStack(Material.COOKED_BEEF, 64);
-        inventory.addItem(beaf);
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+        ItemStack chicken = new ItemStack(Material.COOKED_CHICKEN, 64);
+        inventory.addItem(chicken);
     }
 }
