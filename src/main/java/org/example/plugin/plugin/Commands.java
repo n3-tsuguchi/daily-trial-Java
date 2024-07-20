8fa2c7dff7b24207244799366613d5874d8162ff
津口雄作 <tsuguchi@nnn.or.jp>
2024-07-20 15:42:21 +0900

    commit

--- src/main/java/org/example/plugin/plugin/Commands.java
+++ src/main/java/org/example/plugin/plugin/Commands.java
@@ -6,6 +6,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 
 public class Commands implements CommandExecutor {
     @Override
@@ -46,7 +47,7 @@ public class Commands implements CommandExecutor {
 
             if (args[0].equalsIgnoreCase("shootingstar")) {
                 Player p = (Player) sender;
-                Location location = p.getEyeLocation().add(p.getLocation().getDirection().multiply(20));
+                Location location = p.getEyeLocation().add(p.getLocation().getDirection().multiply(2));
                 p.getWorld().createExplosion(p.getLocation(), 1f, true);
                 p.getWorld().spawnParticle(Particle.FIREWORK, p.getLocation(), 100, 0.5, 0.5, 0.5, 0.1);
                 p.sendMessage("流れ星を落としました");
@@ -61,9 +62,16 @@ public class Commands implements CommandExecutor {
 
             if (args[0].equalsIgnoreCase("breath")) {
                 Player p = (Player) sender;
-                Location location = p.getEyeLocation().add(p.getLocation().getDirection().multiply(20));
+                Location location = p.getEyeLocation().add(p.getLocation().getDirection().multiply(10));
                 p.getWorld().createExplosion(p.getLocation(), 1f, true);
                 p.getWorld().spawnParticle(org.bukkit.Particle.DRAGON_BREATH, location, 100, 0.5, 0.5, 0.5, 0.1);
+
+                Vector direction = location.getDirection();
+                for (double d = 0; d < 10; d += 0.5) {
+                    Location perticleLocation = p.getEyeLocation().add(direction.multiply(d));
+                    p.getWorld().spawnParticle(org.bukkit.Particle.DRAGON_BREATH, perticleLocation, 0);
+                }
+
                 p.sendMessage("ドラゴンブレスを吐きました");
             }
         }
